1f5440494e529862d1bfefc97beab354a7553620
ISHIDA Akio <ishida@coderium.com>
2015-07-26 18:35:10 +0900

    テンプレートエンジンをコンテナ内に格納

--- web/index.php
+++ web/index.php
@@ -4,8 +4,11 @@ require_once __DIR__ .'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function () {
-    $templates = new League\Plates\Engine(__DIR__ . '/../templates');
-    return $templates->render('home', ['title' => 'Hello']);
+$app['view'] = $app->share(function () {
+    return new League\Plates\Engine(__DIR__ . '/../templates');
+});
+
+$app->get('/', function () use ($app) {
+    return $app['view']->render('home', ['title' => 'Hello']);
 });
 $app->run();
