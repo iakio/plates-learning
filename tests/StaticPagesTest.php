7baee5232a98053a20996ecc2aac83783c08cfdc
ISHIDA Akio <ishida@coderium.com>
2015-08-08 18:56:08 +0900

    テストのリファクタリング

--- tests/StaticPagesTest.php
+++ tests/StaticPagesTest.php
@@ -1,6 +1,11 @@
 <?php
 class StaticPagesTest extends \Silex\WebTestCase
 {
+    /** @var  \Symfony\Component\HttpKernel\Client */
+    private $client;
+
+    /** @var  \Symfony\Component\DomCrawler\Crawler */
+    private $crawler;
 
     /**
      * Creates the application.
@@ -12,13 +17,39 @@ class StaticPagesTest extends \Silex\WebTestCase
         return require __DIR__ . '/../src/app.php';
     }
 
+    /**
+     * @param $url
+     * @return $this
+     */
+    private function visit($url) {
+        $this->client = $this->createClient();
+        $this->crawler = $this->client->request('GET', $url);
+        return $this;
+    }
+
+    /**
+     * @param $text
+     * @return $this
+     */
+    private function dontSeeInTitle($text) {
+        $this->assertNotContains($text, $this->crawler->filter('title')->text());
+        return $this;
+    }
+
+    /**
+     * @param $text
+     * @return $this
+     */
+    private function seeInTitle($text) {
+        $this->assertContains($text, $this->crawler->filter('title')->text());
+        return $this;
+    }
+
     /** @test */
     public function home_page_should_not_have_a_custom_page_title() {
-        $client = $this->createClient();
-        $crawler = $client->request('GET', '/');
-        $title = $crawler->filter('title')->text();
-        $this->assertContains('Ruby on Rails', $title);
-        $this->assertNotContains('Home', $title);
+        $this->visit('/')
+            ->seeInTitle('Ruby on Rails')
+            ->dontSeeInTitle('Home');
     }
 
 }
